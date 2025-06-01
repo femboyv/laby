a25b41e6e20abbf12069fafdf916e048a5a5aa6b
femboyv <baumard.louane@gmail.com>
2025-06-01 11:27:53 +0200

    modified:   main.py

--- main.py
+++ main.py
@@ -8,9 +8,7 @@ clock = pygame.time.Clock()
 running = True
 max_fps = 100
 
-player_sprite = pygame.transform.scale(
-    pygame.image.load("91ce98cec1df0f769321571724f1f312.jpg"), (100, 100)
-)
+
 background_sprite = pygame.image.load("d011d6bc1effc67ae5c74bbc5ce090c2.jpg")
 pressed_key = []
 
@@ -21,8 +19,8 @@ y_of_display = 0
 
 class player_class:
     def __init__(self):
-        self.x = screen.get_width() / 2
-        self.y = screen.get_height() / 2
+        self.x = 0
+        self.y = 0
 
         self.angle = pygame.math.Vector2(-1, 0)
         self.max_speed = 7.5
@@ -34,6 +32,16 @@ class player_class:
         self.acceleration_per_tick = self.max_speed / self.tick_to_reach_max_speed
         self.deceleration_per_tick = self.max_speed / self.tick_to_slow_down
 
+        self.height = 100
+        self.rect = pygame.Rect(0, 0, self.height, self.height)
+
+        self.collide_box = self.rect
+
+        self.sprite = pygame.transform.scale(
+            pygame.image.load("91ce98cec1df0f769321571724f1f312.jpg"),
+            (self.height, self.height),
+        )
+
     def draw_self(
         self,
         sprite: pygame.Surface,
@@ -41,7 +49,7 @@ class player_class:
         x: float,
         y: float,
     ):
-        x_relative, y_relative = get_coordinate_relative_to_screen(x, y)
+        self.x_relative, self.y_relative = get_coordinate_relative_to_screen(x, y)
 
         resized_sprite = pygame.transform.rotate(
             sprite,
@@ -51,8 +59,8 @@ class player_class:
             screen,
             resized_sprite,
             (
-                x_relative - (pygame.Surface.get_width(resized_sprite) / 2),
-                y_relative - (pygame.Surface.get_height(resized_sprite) / 2),
+                self.x_relative - (pygame.Surface.get_width(resized_sprite) / 2),
+                self.y_relative - (pygame.Surface.get_height(resized_sprite) / 2),
             ),
         )
 
@@ -72,7 +80,6 @@ class player_class:
         moved_this_tick = True
 
     def decelerate(self, direction: str):
-
         if self.speed.length() < self.deceleration_per_tick and not moved_this_tick:
             self.speed = pygame.Vector2(0, 0)
         else:
@@ -94,6 +101,23 @@ class player_class:
 
                         self.speed[1] += self.deceleration_per_tick
 
+    def update_collision_box(self):
+        self.rect.x = self.x_relative
+        self.rect.y = self.y_relative
+        self.collide_box = self.rect
+        self.collide_box.x += self.speed.x
+        self.collide_box.y += self.speed.y
+        pygame.draw.rect(screen, "red", self.rect)
+
+    def handle_movement(self):
+        self.update_collision_box()
+        if collide_with_wall(self.rect):
+            self.speed.x = 0
+        if collide_with_wall(self.rect):
+            self.speed.y = 0
+        self.x += self.speed.x
+        self.y += self.speed.y
+
 
 def get_coordinate_relative_to_screen(x, y):
     x_relative = x - x_of_display
@@ -102,7 +126,9 @@ def get_coordinate_relative_to_screen(x, y):
 
 
 def set_rect_center_at(rectangle: pygame.Rect, x: float, y: float):
-    return rectangle.move(x - rectangle.width / 2, y - rectangle.height / 2)
+    rectangle.x = x - rectangle.width / 2
+    rectangle.y = y - rectangle.height / 2
+    return rectangle
 
 
 def screen_follow_position(x: float, y: float, strength: float):
@@ -176,12 +202,12 @@ def generate_map():
 
     position = (0, 0)
 
-    for i in range(map_tile_height):  # height
+    for y in range(map_tile_height):  # height
         for _ in range(int(map_tile_width / 2)):  # width
             wall_map.append((position, generate_random_tile()))
             position = (position[0] + 2, position[1])
 
-        position = (i % 2, position[1] + 1)
+        position = (y % 2, position[1] + 1)
 
     return wall_map
 
@@ -194,15 +220,20 @@ def generate_random_tile():
 wall_map = generate_map()
 
 
-def show_tile_by_letter(tile_position: tuple, letter: str):
+def get_walls_by_position(tile_position: tuple):
     global number_of_tile_rendered
+    letter = get_tile_by_position(tile_position)
+    walls_of_tile = []
+
     wall_to_draw = letter[1]
+
     number_of_tile_rendered += 1
     for wall in wall_to_draw:
-        show_wall_by_orientation(tile_position, wall)
+        walls_of_tile.append(get_walls_by_letter(tile_position, wall))
+    return walls_of_tile
 
 
-def show_wall_by_orientation(tile_position: tuple, orientation: int):
+def get_walls_by_letter(tile_position: tuple, orientation: int):
 
     match orientation:  # 0 is up and it's going counterclockwise (i think)
         case 0:
@@ -233,7 +264,8 @@ def show_wall_by_orientation(tile_position: tuple, orientation: int):
                 wall_width,
                 tile_size + wall_width,
             )
-    pygame.draw.rect(screen, "white", rect_to_draw)
+
+    return rect_to_draw
 
 
 def get_tile_by_position(position: tuple):
@@ -251,6 +283,10 @@ def get_all_point_in_rect(position_start: tuple, position_end: tuple):
     return all_point
 
 
+tile_width_of_screen = math.ceil(screen.get_width() / tile_size) + 1
+tile_height_of_screen = math.ceil(screen.get_height() / tile_size) + 1
+
+
 def get_tiles_at_screen():
     x_of_display_in_tile = math.floor(x_of_display / tile_size)
     y_of_display_in_tile = math.floor(y_of_display / tile_size)
@@ -263,22 +299,57 @@ def get_tiles_at_screen():
     )
 
 
+def draw_all_wall(walls_to_draw: list):
+    for wall in walls_to_draw:
+        pygame.draw.rect(screen, "white", wall)
+
+
 player = player_class()
 
-tile_width_of_screen = math.ceil(screen.get_width() / tile_size) + 1
-tile_height_of_screen = math.ceil(screen.get_height() / tile_size) + 1
+
+def set_tile_in_map(position: tuple, tile: tuple):
+    position_in_map = int(position[0] * position[1] / 2)
+    wall_map[position_in_map] = (position, tile)
+
+
+walls_rendered = []
+
+
+def collide_with_wall(
+    collide_box: pygame.Rect = player.collide_box,
+    rects_to_collide_with: list = walls_rendered,
+):
+
+    print(collide_box.collidelist(rects_to_collide_with))
+    if collide_box.collidelist(rects_to_collide_with) == -1:
+        return False
+    else:
+        return True
+
 
 while running:
 
+    walls_rendered = []
+
     number_of_tile_rendered = 0
+
+    set_tile_in_map((0, 1), type_of_wall["NONE"])
+    set_tile_in_map((0, 2), type_of_wall["NONE"])
+    set_tile_in_map((1, 1), type_of_wall["NONE"])
+    set_tile_in_map((1, 2), type_of_wall["NONE"])
 
     tiles_at_screen = get_tiles_at_screen()
 
     for tile_position in tiles_at_screen:
-        show_tile_by_letter(
-            tile_position,
-            get_tile_by_position(tile_position),
-        )
+        walls_rendered += get_walls_by_position(
+            tile_position
+        )  # += used to merge the two lists insted of adding a list in a list with append
+
+    draw_all_wall(walls_rendered)
+
+    player.rect = set_rect_center_at(
+        player.rect, player.x - x_of_display, player.y - y_of_display
+    )
 
     if number_of_tile_rendered != tiles_at_screen.__len__():
         raise ValueError(
@@ -287,9 +358,7 @@ while running:
 
     screen_follow_position(player.x, player.y, 0.75)
 
-    player.draw_self(player_sprite, player.angle, player.x, player.y)
-    pygame.display.flip()
-    screen.fill("black")
+    player.draw_self(player.sprite, player.angle, player.x, player.y)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -308,7 +377,9 @@ while running:
     for key in not_pressed_key:
         player.decelerate(key_mapping(key))
 
-    player.x += player.speed[0]
-    player.y += player.speed[1]
+    player.handle_movement()
+
+    pygame.display.flip()
+    screen.fill("black")
 
     clock.tick_busy_loop(max_fps)
