0e0aaa924e9f0686df04a583611ea14644d9a574
femboyv <baumard.louane@gmail.com>
2025-06-03 23:02:51 +0200

    new file:   actual_map.txt 	modified:   main.py 	modified:   test.py

--- main.py
+++ main.py
@@ -206,31 +206,47 @@ letter_to_walls = {
 }
 
 
-def get_walls_by_letter(letter: str) -> tuple:
-
-    return letter_to_walls[letter]
+def get_walls_by_tile(letter: str) -> tuple:
+    try:
+        return letter_to_walls[letter]
+    except KeyError:
+        return letter_to_walls["f"]  #  f for Null
 
 
 tile_size = 200
 wall_width = 20
-map_tile_height = 50
-map_tile_width = 50
 
 
-def generate_map():
+def read_map_file():
+    with open("actual_map.txt") as file:
+        return file.read()
 
-    wall_map = []
 
-    position = (0, 0)
+scripted_map = read_map_file()
 
-    for _ in range(map_tile_height):  # height
-        for _ in range(map_tile_width):  # width
-            wall_map.append((position, generate_random_tile()))
-            position = (position[0] + 1, position[1])
 
-        position = (0, position[1] + 1)
+def generate_map(script=None, width=50, height=50):
 
-    return wall_map
+    if script != None:
+        world_width_in_tile, world_height_in_tile = (
+            check_for_max_width_and_height_of_map(script)
+        )
+        return (script, world_width_in_tile, world_height_in_tile)
+
+    else:
+
+        wall_map = []
+
+        position = (0, 0)
+
+        for _ in range(height):  # height
+            for _ in range(width):  # width
+                wall_map.append((position, generate_random_tile()))
+                position = (position[0] + 1, position[1])
+
+            position = (0, position[1] + 1)
+
+        return (wall_map, width, height)
 
 
 def generate_random_tile():
@@ -238,82 +254,20 @@ def generate_random_tile():
     return possibility[random.randrange(possibility.__len__())]
 
 
-wall_map = generate_map()
+def check_for_max_width_and_height_of_map(map) -> tuple[int, int]:
+    return (map[len(map) - 1][0][0] + 1, map[len(map) - 1][0][1])
 
-wall_map = [
-    ((0, 0), "i"),
-    ((0, 1), "m"),
-    ((0, 2), "_"),
-    ((0, 3), "_"),
-    ((0, 4), "_"),
-    ((0, 5), "_"),
-    ((0, 6), "_"),
-    ((0, 7), "_"),
-    ((0, 8), "_"),
-    ((0, 9), "_"),
-    ((0, 10), "_"),
-    ((0, 11), "u"),
-    ((0, 12), "_"),
-    ((0, 13), "_"),
-    ((0, 14), "_"),
-    ((0, 15), "_"),
-    ((0, 16), "_"),
-    ((0, 17), "l"),
-    ((1, 0), "i"),
-    ((1, 1), "k"),
-    ((1, 2), "_"),
-    ((1, 3), "_"),
-    ((1, 4), "_"),
-    ((1, 5), "_"),
-    ((1, 6), "_"),
-    ((1, 7), "_"),
-    ((1, 8), "_"),
-    ((1, 9), "_"),
-    ((1, 10), "_"),
-    ((1, 11), "k"),
-    ((1, 12), "_"),
-    ((1, 13), "_"),
-    ((1, 14), "_"),
-    ((1, 15), "_"),
-    ((1, 16), "l"),
-    ((1, 17), "i"),
-    ((2, 0), "i"),
-    ((2, 1), "i"),
-    ((2, 2), "m"),
-    ((2, 3), "_"),
-    ((2, 4), "_"),
-    ((2, 5), "_"),
-    ((2, 6), "_"),
-    ((2, 7), "_"),
-    ((2, 8), "_"),
-    ((2, 9), "l"),
-    ((2, 10), "i"),
-    ((2, 11), "i"),
-    ((2, 12), "m"),
-    ((2, 13), "_"),
-    ((2, 14), "_"),
-    ((2, 15), "_"),
-    ((2, 16), "h"),
-    ((2, 17), "i"),
-    ((3, 0), "i"),
-    ((3, 1), "s"),
-    ((3, 2), "h"),
-    ((3, 3), "_"),
-    ((3, 4), "_"),
-    ((3, 5), "_"),
-    ((3, 6), "_"),
-    ((3, 7), "_"),
-    ((3, 8), "l"),
-    ((3, 9), "i"),
-    ((3, 10), "i"),
-    ((3, 11), "i"),
-    ((3, 12), "i"),
-    ((3, 13), "d"),
-    ((3, 14), "_"),
-    ((3, 15), "_"),
-    ((3, 16), "o"),
-    ((3, 17), "i"),
-]
+
+tile_map, world_width_in_tile, world_height_in_tile = generate_map(
+    scripted_map,
+    18,
+    3,
+)
+
+print(world_width_in_tile, world_height_in_tile)
+
+
+print(tile_map)
 
 
 def get_murs_of_coordinate(tile_position: tuple):
@@ -323,7 +277,7 @@ def get_murs_of_coordinate(tile_position: tuple):
 
     murs_of_tile = []
 
-    walls_to_draw = get_walls_by_letter(letter)
+    walls_to_draw = get_walls_by_tile(letter)
 
     number_of_tile_rendered += 1
 
@@ -369,10 +323,32 @@ def get_mur_from_wall(tile_position: tuple, orientation: int):
 
 
 def get_tile_by_coordinate(coordinate: tuple):
-    coordinate_in_map = int(coordinate[0] * coordinate[1] / 2)
-    tile = wall_map[coordinate_in_map][1]
+    if coordinate[0] >= 0 and coordinate[1] >= 0:
+        coordinate_in_map = int(coordinate[0] + coordinate[1] * world_width_in_tile)
 
-    return tile
+        try:
+            tile_and_position = tile_map[coordinate_in_map]
+        except IndexError:
+            return "f"  # "f" for blank
+
+        if (  # when outside of the map
+            coordinate[0] > world_width_in_tile - 1
+            or coordinate[1] > world_height_in_tile - 1
+        ):
+            return "f"  # "f" for blank
+
+        if tile_and_position[0] != coordinate:
+            raise ValueError(
+                "the coordinate of the output (%d,%d) weren't the coordinate of the input (%d,%d)"
+                % (
+                    tile_map[coordinate_in_map][0][0],
+                    tile_map[coordinate_in_map][0][1],
+                    coordinate[0],
+                    coordinate[1],
+                )
+            )
+
+        return tile_and_position[1]
 
 
 def get_all_point_in_rect(position_start: tuple, position_end: tuple):
@@ -391,6 +367,7 @@ height_of_screen_in_tile = math.ceil(screen.get_height() / tile_size) + 1
 def get_tiles_at_screen():
     x_of_display_in_tile = math.floor(x_of_display / tile_size)
     y_of_display_in_tile = math.floor(y_of_display / tile_size)
+
     return get_all_point_in_rect(
         (x_of_display_in_tile, y_of_display_in_tile),
         (
@@ -410,22 +387,10 @@ player = player_class()
 
 def set_tile_in_map(position: tuple, tile: tuple):
     position_in_map = int(position[0] * position[1] / 2)
-    wall_map[position_in_map] = (position, tile)
+    tile_map[position_in_map] = (position, tile)
 
 
 murs_rendered = []
-
-
-def collide_with_wall(
-    collide_box: pygame.Rect = player.collide_box,
-    rects_to_collide_with: list = murs_rendered,
-):
-
-    if collide_box.collidelist(rects_to_collide_with) == -1:
-        return False
-    else:
-        print("touché")
-        return True
 
 
 def closest_to(number: float, first_number: float, second_number: float):
@@ -437,91 +402,157 @@ def closest_to(number: float, first_number: float, second_number: float):
         return second_number
 
 
-# set_tile_in_map((0, 1), type_of_wall["NONE"])
-# set_tile_in_map((0, 2), type_of_wall["NONE"])
-# set_tile_in_map((1, 1), type_of_wall["NONE"])
-# set_tile_in_map((1, 2), type_of_wall["NONE"])
+def show_map():
+    global normal_view
+    global map_view
+    normal_view = False
+    map_view = True
 
+
+def hide_map():
+    global normal_view
+    global map_view
+    normal_view = True
+    map_view = False
+
+
+def define_map_surface() -> pygame.Surface:
+    output_surface = pygame.Surface(
+        (world_width_in_tile * tile_size, world_height_in_tile * tile_size)
+    )
+    all_murs = []
+    for tile in tile_map:
+        for wall in get_walls_by_tile(tile[1]):
+            all_murs.append(get_mur_from_wall(tile[0], wall))
+            pygame.draw.rect(output_surface, "white", get_mur_from_wall(tile[0], wall))
+
+    return pygame.transform.rotate(
+        pygame.transform.scale(
+            output_surface,
+            (
+                pygame.math.clamp(
+                    min(screen.get_width(), screen.get_height())
+                    * (world_width_in_tile / world_height_in_tile),
+                    0,
+                    min(screen.get_width(), screen.get_height()),
+                ),
+                pygame.math.clamp(
+                    min(screen.get_width(), screen.get_height())
+                    * (world_height_in_tile / world_width_in_tile),
+                    0,
+                    min(screen.get_width(), screen.get_height()),
+                ),
+            ),
+        ),
+        0,
+    )
+
+
+map_surface = define_map_surface()
+
+normal_view = True
+map_view = False
 
 while running:
+    if normal_view:
+        murs_rendered = []
 
-    murs_rendered = []
+        number_of_tile_rendered = 0
 
-    number_of_tile_rendered = 0
+        tiles_at_screen = get_tiles_at_screen()
+        for tile_position in tiles_at_screen:
 
-    tiles_at_screen = get_tiles_at_screen()
+            murs_rendered += get_murs_of_coordinate(
+                tile_position
+            )  # += used to merge the two lists insted of adding a list in a list with append
 
-    for tile_position in tiles_at_screen:
+        draw_all_wall(murs_rendered)
 
-        murs_rendered += get_murs_of_coordinate(
-            tile_position
-        )  # += used to merge the two lists insted of adding a list in a list with append
+        if number_of_tile_rendered != tiles_at_screen.__len__():
+            raise ValueError(
+                "number_of_rendered_tiles can't be more than number of tiles_at_screen"
+            )
 
-    draw_all_wall(murs_rendered)
+        screen_follow_position(player.x, player.y, 0.75)
 
-    if number_of_tile_rendered != tiles_at_screen.__len__():
-        raise ValueError(
-            "number_of_rendered_tiles can't be more than number of tiles_at_screen"
-        )
+        player.draw_self(player.sprite, player.angle, player.x, player.y)
 
-    screen_follow_position(player.x, player.y, 0.75)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    show_map()
+                else:
+                    pressed_key.append(pygame.key.name(event.key))
 
-    player.draw_self(player.sprite, player.angle, player.x, player.y)
+            if event.type == pygame.KEYUP and event.key != pygame.K_m:
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == pygame.KEYDOWN:
-            pressed_key.append(pygame.key.name(event.key))
-        if event.type == pygame.KEYUP:
-            pressed_key.pop(pressed_key.index(pygame.key.name(event.key)))
+                pressed_key.pop(pressed_key.index(pygame.key.name(event.key)))
 
-    not_pressed_key = ["z", "q", "s", "d"]
-    moved_this_tick = False
-    for key in pressed_key:
-        player.accelerate(key_mapping(key))
-        not_pressed_key.pop(not_pressed_key.index(key))
+        not_pressed_key = ["z", "q", "s", "d"]
+        moved_this_tick = False
+        for key in pressed_key:
+            player.accelerate(key_mapping(key))
+            not_pressed_key.pop(not_pressed_key.index(key))
 
-    for key in not_pressed_key:
+        for key in not_pressed_key:
+            player.decelerate(key_mapping(key))
 
-        player.decelerate(key_mapping(key))
+        if player.speed.x != 0:
 
-    if player.speed.x != 0:
+            player.collide_box.x = (
+                player.x_relative
+                + closest_to(player.speed.x, player.max_speed, -player.max_speed)
+                - player.height / 2
+            )
+            player.collide_box.y = (
+                player.y_relative
+                - player.height / 2
+                + closest_to(player.speed.x, player.max_speed, -player.max_speed)
+            )
 
-        player.collide_box.x = (
-            player.x_relative
-            + closest_to(player.speed.x, player.max_speed, -player.max_speed)
-            - player.height / 2
-        )
-        player.collide_box.y = (
-            player.y_relative
-            - player.height / 2
-            + closest_to(player.speed.x, player.max_speed, -player.max_speed)
-        )
+            if player.collide_box.collidelist(murs_rendered) == -1:
+                player.x += player.speed.x
+            else:
+                player.speed.x = 0
 
-        if player.collide_box.collidelist(murs_rendered) == -1:
-            player.x += player.speed.x
-        else:
-            player.speed.x = 0
+        if player.speed.y != 0:
 
-    if player.speed.y != 0:
+            player.collide_box.x = (
+                player.x_relative
+                + closest_to(player.speed.x, player.max_speed, -player.max_speed)
+                - player.height / 2
+            )
+            player.collide_box.y = (
+                player.y_relative
+                - player.height / 2
+                + closest_to(player.speed.x, player.max_speed, -player.max_speed)
+            )
 
-        player.collide_box.x = (
-            player.x_relative
-            + closest_to(player.speed.x, player.max_speed, -player.max_speed)
-            - player.height / 2
-        )
-        player.collide_box.y = (
-            player.y_relative
-            - player.height / 2
-            + closest_to(player.speed.x, player.max_speed, -player.max_speed)
-        )
+            if player.collide_box.collidelist(murs_rendered) == -1:
+                player.y += player.speed.y
+            else:
+                player.speed.y = 0
 
-        if player.collide_box.collidelist(murs_rendered) == -1:
-            player.y += player.speed.y
-        else:
-            player.speed.y = 0
+    if map_view:
+        pygame.Surface.blit(screen, map_surface, (0, 0))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_m:
+                hide_map()
 
     pygame.display.flip()
     screen.fill("black")
     clock.tick_busy_loop(max_fps)
+
+"""
+bug report:
+reading the file need json load but idk syntax
+bug at map showing only part of the screen
+bug at map when holding key and pressing map key
+bug at collision for vertical collision
+
+
+"""
